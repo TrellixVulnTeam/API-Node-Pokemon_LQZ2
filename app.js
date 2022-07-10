c07d44e2edaac70b80476004370e30e524de028d
Clément BASCOP <clement.bascop72@gmail.com>
2022-07-10 23:18:41 +0200

    mise en prod

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const morgan = require('morgan');
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
 const sequelize = require('./src/db/sequelize')
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app
     .use(favicon(__dirname + '/favicon.ico'))
-    .use(morgan('dev')) //Dependance morgan permettant d'afficher les logger dans la console. (Type de requete + URL + code de statut)
     .use(bodyParser.json())
 
 sequelize.initDb()
+
+app.get('/', (req, res) => {
+    res.json('Hello, Heroku')
+})
 
 //Ici, futur point de terminaisons
 require('./src/routes/findAllPokemons')(app);
